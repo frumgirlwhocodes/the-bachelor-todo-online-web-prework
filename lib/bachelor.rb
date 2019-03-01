f10401963837f83781011c2c9be66f53928bac2a
lieby weintraub <liebahinda@gmail.com>
2019-03-01 18:14:46 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -2,7 +2,7 @@ def get_first_name_of_season_winner(data, season)
   # code here
   data[season].each do |contestant|
     if contestant["status"] == "winner"
-     return contestant["name"]
+     return contestant["name"].join("")
    end 
   
 end

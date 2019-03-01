8eb5eb9a6c037bc6713e8549d076da5c2cd56f05
lieby weintraub <liebahinda@gmail.com>
2019-03-01 18:23:06 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -4,7 +4,7 @@ def get_first_name_of_season_winner(data, season)
     if contestant["status"] == "winner"
      return contestant["name"].split[0]
    end 
-  
+  end
 end
 
 def get_contestant_name(data, occupation)
